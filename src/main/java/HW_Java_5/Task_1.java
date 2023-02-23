b9e48c5924376712815047d295a5dbce73a43277
Anton Manuylov <killsonic4@gmail.com>
2023-02-23 16:40:08 +0300

    initial commit

---
+++ src/main/java/HW_Java_5/Task_1.java
@@ -0,0 +1,78 @@
+package HW_Java_5;
+
+// 2. Пусть дан список сотрудников:
+// Иван Иванов
+// Светлана Петрова
+// Кристина Белова
+// Анна Мусина
+// Анна Крутова
+// Иван Юрин
+// Петр Лыков
+// Павел Чернов
+// Петр Чернышов
+// Мария Федорова
+// Марина Светлова
+// Мария Савина
+// Мария Рыкова
+// Марина Лугова
+// Анна Владимирова
+// Иван Мечников
+// Петр Петин
+// Иван Ежов
+// Написать программу, которая найдет и выведет повторяющиеся имена
+// с количеством повторений. Отсортировать по убыванию популярности.
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        Map<String,Integer> nameMap = new HashMap<>();
+        String employees = "Иван Иванов " +
+                "Светлана Петрова " +
+                "Кристина Белова " +
+                "Анна Мусина " +
+                "Анна Крутова " +
+                "Иван Юрин " +
+                "Петр Лыков " +
+                "Павел Чернов " +
+                "Петр Чернышов " +
+                "Мария Федорова " +
+                "Марина Светлова " +
+                "Мария Савина " +
+                "Мария Рыкова " +
+                "Марина Лугова " +
+                "Анна Владимирова " +
+                "Иван Мечников " +
+                "Петр Петин " +
+                "Иван Ежов ";
+        String[] listofNamesAndSurnames = employees.split(" ");
+        for (int i = 0; i < listofNamesAndSurnames.length; i+=2) {
+            if(nameMap.containsKey(listofNamesAndSurnames[i])){
+                nameMap.replace(listofNamesAndSurnames[i], nameMap.get(listofNamesAndSurnames[i]) + 1);
+            } else{
+                nameMap.put(listofNamesAndSurnames[i], 1);
+            }
+        }
+
+        System.out.println(nameMap);
+        Map<String,Integer> sortedNameMap = new LinkedHashMap<>();
+        int max = 1;
+        for(int value: nameMap.values()){
+            if (value > max) {
+                max = value;
+            }
+        }
+
+        for (int i = max; i > 0; i--) {
+            for (Map.Entry<String, Integer> entry :nameMap.entrySet()) {
+                String key = entry.getKey();
+                if (nameMap.get(key) == i) {
+                    sortedNameMap.put(key, nameMap.get(key));
+                }
+            }
+        }
+        System.out.println(sortedNameMap);
+    }
+}
